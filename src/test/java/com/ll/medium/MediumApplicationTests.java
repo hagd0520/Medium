ccc07ba59b8a0c602b44c37ecc0dfc7206d14cef
hagd <hagd0520@gmail.com>
2023-12-15 09:34:39 +0900

    init

---
+++ src/test/java/com/ll/medium/MediumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.medium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
