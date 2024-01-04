25d107667c246a10d78d4650185cd4337336668d
hagd <hagd0520@gmail.com>
2024-01-04 15:02:49 +0900

    articleComment Entity 추가

--- src/main/java/com/ll/medium/domain/article/article/entity/Article.java
+++ src/main/java/com/ll/medium/domain/article/article/entity/Article.java
@@ -1,12 +1,18 @@
 package com.ll.medium.domain.article.article.entity;
 
+import com.ll.medium.domain.article.articleComment.entity.ArticleComment;
 import com.ll.medium.domain.member.member.entity.Member;
 import com.ll.medium.global.jpa.BaseEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static jakarta.persistence.CascadeType.ALL;
 import static jakarta.persistence.FetchType.LAZY;
 import static lombok.AccessLevel.PROTECTED;
 
@@ -25,6 +31,9 @@ public class Article extends BaseEntity {
     private long hit;
     private boolean isPublished;
     private boolean isPaid;
+    @OneToMany(mappedBy = "article", cascade = ALL, orphanRemoval = true)
+    @Builder.Default
+    private List<ArticleComment> ArticleComments = new ArrayList<>();
 
     public boolean isPaid() {
         return isPaid;
@@ -32,5 +41,13 @@ public class Article extends BaseEntity {
 
     public void addHit() {
         hit = hit + 1;
+    }
+
+    public void addArticleComment(Member author, String body) {
+        ArticleComment articleComment = ArticleComment.builder()
+                .article(this)
+                .author(author)
+                .body(body)
+                .build();
     }
 }
---
+++ src/main/java/com/ll/medium/domain/article/articleComment/entity/ArticleComment.java
@@ -0,0 +1,27 @@
+package com.ll.medium.domain.article.articleComment.entity;
+
+import com.ll.medium.domain.article.article.entity.Article;
+import com.ll.medium.domain.member.member.entity.Member;
+import com.ll.medium.global.jpa.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import static jakarta.persistence.FetchType.LAZY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@SuperBuilder
+@AllArgsConstructor(access = PROTECTED)
+@NoArgsConstructor(access = PROTECTED)
+@Getter
+@Setter
+@ToString(callSuper = true)
+public class ArticleComment extends BaseEntity {
+    @ManyToOne(fetch = LAZY)
+    private Article article;
+    @ManyToOne(fetch = LAZY)
+    private Member author;
+    private String body;
+}
